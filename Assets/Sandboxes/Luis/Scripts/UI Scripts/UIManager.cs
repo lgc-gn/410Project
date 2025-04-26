03ab3780b6ab813be92f8aa5ea8bc2ae3f0a64f3
lgc-gn <guzmancornejoluis@gmail.com>
2025-04-26 15:34:46 -0700

    Updates

--- Assets/Sandboxes/Luis/Scripts/UI Scripts/UIManager.cs
+++ Assets/Sandboxes/Luis/Scripts/UI Scripts/UIManager.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
 
-    public GameObject infoPanel;
-    public Text nameText;
-    public Text classText;
-    public Text hpText;
+    public Canvas playerHUD;
+    public TMP_Text unitNameText;
+    public TMP_Text classText;
 
     void Awake()
     {
@@ -21,7 +21,8 @@ public class UIManager : MonoBehaviour
     public void ShowUnitInfo(Unit unit)
     {
         //print($"Name: {data.characterName}\nClass: {data.characterClass}\nHealth: {data.currentHealth}\nLevel: {data.currentLevel}");
-        print("Hovered");
+        unitNameText.SetText(unit.data.name);
+        classText.SetText(unit.data.characterClass);
 
     }
 
