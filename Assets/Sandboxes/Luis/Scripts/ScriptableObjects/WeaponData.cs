c202dfa9c0c390548c6ce2c711ae2e064cc95447
lgc-gn <guzmancornejoluis@gmail.com>
2025-05-13 13:51:05 -0700

    Equipment system setup

--- Assets/Sandboxes/Luis/Scripts/ScriptableObjects/WeaponData.cs
+++ Assets/Sandboxes/Luis/Scripts/ScriptableObjects/WeaponData.cs
@@ -1,8 +1,46 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewClassData", menuName = "RPG Objects/Weapon Data")]
 public class WeaponData : ScriptableObject
 {
-    public string weaponName, weaponDescription;
-    public int baseDamage;
+
+    public enum WeaponType{
+        Dagger,
+        Spear,
+        Sword,
+        Axe,
+        Greatsword,
+        Staff,
+        Mace
+    }
+
+    [Header("Identifiers")]
+    public string weaponName;
+    public string weaponDescription;
+    public WeaponType weaponType;
+    public GameObject weaponModel;
+
+    [Header("Animation Data")]
+
+    public List<AnimationClip> animations;
+
+    [Header("Combat Data")]
+    public float baseDamage;
+    public float critChance = 5f;
+    public float critMultiplier = 1.2f;
+    public float flatArmorPen = 0f;
+    public float percentArmorPen = 0f;
+
+    [Header("Scalings")]
+    // Initialize all to 1.0, no scaling provided by default
+    // This affects WEAPON damage, not spells
+    public float strScaling = 1.0f;
+    public float dexScaling = 1.0f;
+    public float intScaling = 1.0f;
+    public float wisScaling = 1.0f;
+
+
 }
